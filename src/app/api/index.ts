276db190855124cd6450ef0787272ed6922c6f29
Vadym Marukhnenko <vdm.marukhnenko@gmail.com>
2024-01-04 12:31:32 +0000

    init app

---
+++ src/app/api/index.ts
@@ -0,0 +1,9 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000/api",
+  }),
+  endpoints: () => ({}),
+});
